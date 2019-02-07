493f216b06f3fbe9cd71ae38063d576e61e89b16
BatsGoScreechh <bird11@mctc.edu>
2019-02-07 10:43:17 -0500

    Fixed search

--- src/scripts/concerts.js
+++ src/scripts/concerts.js
@@ -1,27 +1,46 @@
 
-const searchConcerts = concertParam => {
-    document.querySelector("#resultsContainer").innerHTML = "";
+const searchConcerts = (concertParam) => {
+    let concertHTML = "";
 
-    // const concertResults = (entries.name, entries._embedded.venues[0].name) => {
-    //     return `<div><h2>${entries.name}</h2><p>${entries._embedded.venues[0].name}</p></div>`
-    // }
+    fetch(`https://app.ticketmaster.com/discovery/v2/events.json?classificationName=music&stateCode=TN&keyword=${concertParam}&apikey=7OVjc7Dz2PXDeHNgQ25DgvxAae5oorJM`) // Fetch from the API
+        .then(response => response.json())  // Parse as JSON
+        .then(parsedEntries => {
+            parsedEntries._embedded.events.slice(-4).forEach(entries => {
 
-fetch(`https://app.ticketmaster.com/discovery/v2/events.json?classificationName=music&stateCode=TN&keyword=${concertParam}&apikey=7OVjc7Dz2PXDeHNgQ25DgvxAae5oorJM`) // Fetch from the API
-    .then(response => response.json())  // Parse as JSON
-    .then(parsedEntries => {
-        parsedEntries._embedded.events.forEach(entries => {
-            
-            document.querySelector("#resultsContainer").innerHTML += `<div><h2>${entries.name}</h2><p>${entries._embedded.venues[0].name}</p></div>`
-        })
-    })
+                concertHTML += `<h4>${entries.name} at ${entries._embedded.venues[0].name}</h4><button id="save-button">Save</button>`
+            })
 
-    
-document.querySelector("#searchConcertBtn").addEventListener("click", () => {
-    // get the user's input
-    const searchTerm = document.querySelector("#searchConcert").value;
-    // log it to the console
-    searchConcerts(searchTerm);
-    document.querySelector("#resultsContainer").innerHTML = searchConcerts();
-})
+            document.querySelector("#resultsContainer").innerHTML += concertHTML;
+        });
 }
 
+
+
+document.querySelector("#searchConcertBtn").addEventListener("click", () => {
+    const searchTerm = document.querySelector("#searchConcert").value;
+    searchConcerts(searchTerm);
+});
+
+
+
+// document.querySelector("#resultsContainer").addEventListener("click", () => {
+//     const searchTerm = document.querySelector("#searchConcert").value;
+//     if (event.target.id === "save-button") {
+//         document.querySelector("#itineraryContainer").innerHTML = searchConcerts(searchTerm);
+//         document.querySelector("#resultsContainer").innerHTML = "";
+//     }
+// }
+// )
+
+document.querySelector("#resultsContainer").addEventListener("click", () => {
+    const concertSearch =  event.target.parentNode;
+    if(event.target.id === "save-button"){
+        console.log("You clicked on the button!");
+        const concertCard = concertSearch.cloneNode(true);
+        document.querySelector("#itineraryContainer").appendChild(concertCard);
+        concertSearch.remove();
+    // } else if (event.target.id === "not-btn"){
+    //     console.log("you clicked on the not button!")
+    //     concertSearch.remove()
+    }
+})
