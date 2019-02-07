11b5bf42706384aff17788f75afb477294aec897
Cluttered04 <sbpwem@gmail.com>
2019-02-07 10:41:46 -0500

    Saved/remove to itinerary

--- src/scripts/parks.js
+++ src/scripts/parks.js
@@ -1,32 +1,44 @@
 const searchParks = (taco) => {
     let parkHTML = "";
-    
+
     fetch(`https://data.nashville.gov/resource/xbru-cfzi.json?$$app_token=VYe8wkkThsk0EjGHsubsD5hTx&&${taco}=Yes`)
         .then(parks => parks.json())
         .then(parsedParks => {
-            parsedParks.forEach(parkInfo => {
-                parkHTML += `<p>${parkInfo.park_name} ${parkInfo.mapped_location_address}</p><button id="save-button">Save</button>`
+            console.log(parsedParks);
+            parsedParks.slice(-4).forEach(parkInfo => {
+                parkHTML += `<div class="individual-park"><p>${parkInfo.park_name} ${parkInfo.mapped_location_address}</p><button id="save-button">Save</button></div>`
             })
             document.querySelector("#resultsContainer").innerHTML += parkHTML;
 
         });
     }
-    
-    
-    
+
+
     document.querySelector("#searchButton1").addEventListener("click", () => {
         const parkSearch = document.querySelector("#searchInput1").value;
         searchParks(parkSearch);
     });
 
 
-
+    const removeButton = document.createElement('button');
+    removeButton.setAttribute("id", "remove-button");
+    removeButton.innerHTML = "Remove";
     document.querySelector("#resultsContainer").addEventListener("click", () => {
-        const parkSearch = document.querySelector("#searchInput1").value;
         console.log("You clicked!");
+        const addToItinerary = event.target.parentNode;
         if(event.target.id === "save-button") {
-            document.querySelector("#itineraryContainer").innerHTML = searchParks(parkSearch);
+            const itineraryPark = addToItinerary.cloneNode(true);
             document.querySelector("#resultsContainer").innerHTML = "";
-        }
+            document.querySelector("#parkItineraryContainer").innerHTML = "";
+            document.querySelector("#parkItineraryContainer").appendChild(itineraryPark);
+            document.querySelector("#save-button").remove();
+            document.querySelector(".individual-park").appendChild(removeButton);
+        }   
     }
     )
+
+    document.querySelector("#parkItineraryContainer").addEventListener("click", () => {
+        if(event.target.id === "remove-button") {
+            document.querySelector("#parkItineraryContainer").innerHTML = "";
+        }
+    })
