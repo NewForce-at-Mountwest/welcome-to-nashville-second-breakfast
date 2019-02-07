e3cf429e95668977dd6916381e16d0a10dc3d033
BatsGoScreechh <bird11@mctc.edu>
2019-02-06 23:14:39 -0500

    Eh

--- src/scripts/concerts.js
+++ src/scripts/concerts.js
@@ -2,15 +2,17 @@
 const searchConcerts = concertParam => {
     document.querySelector("#resultsContainer").innerHTML = "";
 
+    // const concertResults = (entries.name, entries._embedded.venues[0].name) => {
+    //     return `<div><h2>${entries.name}</h2><p>${entries._embedded.venues[0].name}</p></div>`
+    // }
 
-fetch(`https://app.ticketmaster.com/discovery/v2/events.json?classificationName=music&stateCode=TN&genre=${concertParam}apikey=7OVjc7Dz2PXDeHNgQ25DgvxAae5oorJM`) // Fetch from the API
+fetch(`https://app.ticketmaster.com/discovery/v2/events.json?classificationName=music&stateCode=TN&keyword=${concertParam}&apikey=7OVjc7Dz2PXDeHNgQ25DgvxAae5oorJM`) // Fetch from the API
     .then(response => response.json())  // Parse as JSON
     .then(parsedEntries => {
         parsedEntries._embedded.events.forEach(entries => {
+            
             document.querySelector("#resultsContainer").innerHTML += `<div><h2>${entries.name}</h2><p>${entries._embedded.venues[0].name}</p></div>`
         })
-
-
     })
 
     
@@ -21,3 +23,5 @@ document.querySelector("#searchConcertBtn").addEventListener("click", () => {
     searchConcerts(searchTerm);
     document.querySelector("#resultsContainer").innerHTML = searchConcerts();
 })
+}
+
