26089e897abdcebf447d1e55e924da597edbdb02
Newforce <nicholasandrewmitchell@gmail.com>
2019-02-07 11:45:48 -0500

    merging master

--- src/scripts/restaurants.js
+++ src/scripts/restaurants.js
@@ -1,30 +1,3 @@
-// const printRestaurantCard = () => {
-//     const YES = "This is doing something";
-//     document.querySelector(
-//       "#search-results"
-//     ).innerHTML += YES;
-// }
-
-// const rS = document.querySelector("#search-results2").value;
-
-// const searchRestaurants = (value) => {
-//     document.querySelector("#search-results2").innerHTML;
-//     fetch(`https://developers.zomato.com/api/v2.1/search?entity_id=1138&q=${value}&count=4&sort=rating&order=desc&apikey=83b2f03cc93d8559e02be467e17c3440`
-//     )
-//       .then(search  => search.json()) // convert them to a JS object
-//       .then(restaurantSearch => {
-//             restaurantSearch.forEach(rS => {
-//                 parkHTML += `<p>${rS.restaurant.name}</p><button>Add</button`
-//             })
-//             document.querySelector("#resultsContainer").innerHTML += parkHTML;
-//       });
-//   };
-
-//   document.querySelector("#search-results2").addEventListener("click", searchRestaurants);
-
-//   console.log(searchRestaurants(rS));
-//   console.log(rS);
-
 document.querySelector("#searchButton2").addEventListener("click", () => {
   const food = document.querySelector("#searchInput2").value;
   findRestaurants(food);
@@ -45,12 +18,48 @@ const findRestaurants = value => {
          document.querySelector("#resultsContainer").innerHTML += restaurantHTML;
       });
     };
-// document.querySelector("#resultsContainer").addEventListener("click", () => {
-//   const parkSearch = document.querySelector("#searchInput2").value;
-//   if (event.target.id === "save-button") {
-//     document.querySelector("#itineraryContainer").innerHTML = searchParks(
-//       parkSearch
-//     );
-//     document.querySelector("#resultsContainer").innerHTML = "";
-//   }
+
+
+// document.querySelector("#searchButton2").addEventListener("click", () => {
+//   const food = document.querySelector("#searchInput2").value;
+//   findRestaurants(food);
 // });
+
+// const findRestaurants = value => {
+//   let restaurantHTML = "";
+
+//   fetch(
+//     `https://developers.zomato.com/api/v2.1/search?entity_id=1138&q=${value}&count=4&sort=rating&order=desc&apikey=83b2f03cc93d8559e02be467e17c3440`
+//   )
+//     .then(restaurant => restaurant.json())
+//     .then(parsedRestaurant => {
+//         console.log(parsedRestaurant);
+//         for(let i=0; i <restaurants.length; i++){
+//         restaurantHTML += `<h3>${restaurants[i].restaurant.name}</h3><p>${restaurants[i].restaurant.url}</p>`
+//         };
+//          document.querySelector("#resultsContainer").innerHTML += restaurantHTML;
+//       });
+//     };
+
+const removeButton2 = document.createElement('button2');
+    removeButton2.setAttribute("class", "remove-button2");
+    removeButton2.innerHTML = "Remove2";
+    document.querySelector("#resultsContainer").addEventListener("click", () => {
+        console.log("You clicked!");
+        const addToItinerary2 = event.target.parentNode;
+        if(event.target.id === "save-button2") {
+            const itineraryPark2 = addToItinerary2.cloneNode(true);
+            document.querySelector("#resultsContainer").innerHTML = "";
+            document.querySelector("#parkItineraryContainer2").innerHTML = "";
+            document.querySelector("#parkItineraryContainer2").appendChild(itineraryPark2);
+            document.querySelector("#save-button2").remove();
+            document.querySelector(".individual-park2").appendChild(removeButton2);
+        }   
+    }
+    )
+
+    document.querySelector("#parkItineraryContainer2").addEventListener("click", () => {
+        if(event.target.class === "remove-button2") {
+            document.querySelector("#parkItineraryContainer2").innerHTML = "";
+        }
+    })
