1cb746f7c5d082fcd79246b7594178cbcccaa8c8
Newforce <nicholasandrewmitchell@gmail.com>
2019-02-08 09:39:03 -0500

    Yes

--- src/scripts/meetup.js
+++ src/scripts/meetup.js
@@ -1,51 +1,60 @@
 const searchMeetups = meetUpParam => {
-    document.querySelector("#resultsContainer").innerHTML = "";   
-fetch(`https://www.eventbriteapi.com/v3/events/search/?q=nashville_${meetUpParam}&token=WZJG657L2ZKPAVFFGYPQ`, {
-        headers: {
-          "Authorization": `Bearer WZJG657L2ZKPAVFFGYPQ`,
-          "Accept": "application/json"
-        }}
-)
-    .then(function(allMeetups){
-        return allMeetups.json()
+  document.querySelector("#resultsContainer").innerHTML = "";
+  fetch(
+    `https://www.eventbriteapi.com/v3/events/search/?q=nashville_${meetUpParam}&token=WZJG657L2ZKPAVFFGYPQ`,
+    {
+      headers: {
+        Authorization: `Bearer WZJG657L2ZKPAVFFGYPQ`,
+        Accept: "application/json"
+      }
+    }
+  )
+    .then(function(allMeetups) {
+      return allMeetups.json();
     })
     .then(function(parsedMeetups) {
-        console.log(parsedMeetups);
-        parsedMeetups.events.slice(-4).forEach(event =>{
-        document.querySelector("#resultsContainer").innerHTML += 
-        `<div class="individual-meetup"><h3>${event.name.text}</h3><button id="saveButton3">Save</button></div>`
+      console.log(parsedMeetups);
+      parsedMeetups.events.slice(-4).forEach(event => {
+        document.querySelector(
+          "#resultsContainer"
+        ).innerHTML += `<div class="individual-meetup"><h3>${
+          event.name.text
+        }</h3><button id="saveButton3">Save</button></div>`;
         // <a href=${event.url}>${event.url}</a></div>`<p>${event.description.text}</p>`
-        })
-   }
-)}
+      });
+    });
+};
 
 document.querySelector("#searchButton3").addEventListener("click", () => {
-    document.querySelector("#resultsContainer").innerHTML = "";
-    const meetUpParam = document.querySelector("#searchInput3").value;
-    // log it to the console
-    console.log(meetUpParam);
-    searchMeetups(`${meetUpParam}`);
-}) 
+  document.querySelector("#resultsContainer").innerHTML = "";
+  const meetUpParam = document.querySelector("#searchInput3").value;
+  // log it to the console
+  console.log(meetUpParam);
+  searchMeetups(`${meetUpParam}`);
+});
 
-const removeButton3 = document.createElement('button');
+const removeButton3 = document.createElement("button");
 removeButton3.setAttribute("id", "remove-button3");
 removeButton3.innerHTML = "Remove";
 document.querySelector("#resultsContainer").addEventListener("click", () => {
-    // console.log("You clicked!");
-    const addToItinerary3 = event.target.parentNode;
-    if(event.target.id === "saveButton3") {
-        const itineraryMeetup = addToItinerary3.cloneNode(true);
-        document.querySelector("#resultsContainer").innerHTML = "";
-        document.querySelector("#meetupItineraryContainer").innerHTML = "";
-        document.querySelector("#meetupItineraryContainer").appendChild(itineraryMeetup);
-        document.querySelector("#saveButton3").remove();
-        document.querySelector(".individual-meetup").appendChild(removeButton3);
-    }   
-}
-)
+  // console.log("You clicked!");
+  const addToItinerary3 = event.target.parentNode;
+  if (event.target.id === "saveButton3") {
+    const itineraryMeetup = addToItinerary3.cloneNode(true);
+    document.querySelector("#resultsContainer").innerHTML = "";
+    document.querySelector("#meetupItineraryContainer").innerHTML = "";
+    document
+      .querySelector("#meetupItineraryContainer")
+      .appendChild(itineraryMeetup);
+    document.querySelector("#saveButton3").remove();
+    document.querySelector(".individual-meetup").appendChild(removeButton3);
+  }
+});
 
-document.querySelector("#meetupItineraryContainer").addEventListener("click", () => {
-    if(event.target.id === "remove-button3") {
-        document.querySelector("#meetupItineraryContainer").innerHTML = "";
+document
+  .querySelector("#meetupItineraryContainer")
+  .addEventListener("click", () => {
+    if (event.target.id === "remove-button3") {
+      document.querySelector("#meetupItineraryContainer").innerHTML = "";
     }
-})
+  });
--- src/scripts/restaurants.js
+++ src/scripts/restaurants.js
@@ -1,5 +1,5 @@
 document.querySelector("#searchButton2").addEventListener("click", () => {
-document.querySelector("#resultsContainer").innerHTML = "";
+  document.querySelector("#resultsContainer").innerHTML = "";
   const food = document.querySelector("#searchInput2").value;
   findRestaurants(food);
 });
@@ -12,32 +12,39 @@ const findRestaurants = value => {
   )
     .then(restaurant => restaurant.json())
     .then(parsedRestaurant => {
-        console.log(parsedRestaurant);
-        for(let i=0; i <parsedRestaurant.restaurants.length; i++){
-        restaurantHTML += `<div class="individual-park2"><h3>${parsedRestaurant.restaurants[i].restaurant.name}</h3><p>${parsedRestaurant.restaurants[i].restaurant.url}</p><button id="save-button2">Save</button></div>`
-        };
-         document.querySelector("#resultsContainer").innerHTML += restaurantHTML;
-      });
-    };
+      console.log(parsedRestaurant);
+      for (let i = 0; i < parsedRestaurant.restaurants.length; i++) {
+        restaurantHTML += `<div class="individual-park2"><h3>${
+          parsedRestaurant.restaurants[i].restaurant.name
+        }</h3><p>${
+          parsedRestaurant.restaurants[i].restaurant.url
+        }</p><button id="save-button2">Save</button></div>`;
+      }
+      document.querySelector("#resultsContainer").innerHTML += restaurantHTML;
+    });
+};
 
-const removeButton2 = document.createElement('button');
-    removeButton2.setAttribute("id", "remove-button2");
-    removeButton2.innerHTML = "Remove";
-    document.querySelector("#resultsContainer").addEventListener("click", () => {
-        const addToItinerary2 = event.target.parentNode;
-        if(event.target.id === "save-button2") {
-            const itineraryPark2 = addToItinerary2.cloneNode(true);
-            document.querySelector("#resultsContainer").innerHTML = "";
-            document.querySelector("#parkItineraryContainer2").innerHTML = "";
-            document.querySelector("#parkItineraryContainer2").appendChild(itineraryPark2);
-            document.querySelector("#save-button2").remove();
-            document.querySelector(".individual-park2").appendChild(removeButton2);
-        }   
+const removeButton2 = document.createElement("button");
+removeButton2.setAttribute("id", "remove-button2");
+removeButton2.innerHTML = "Remove";
+document.querySelector("#resultsContainer").addEventListener("click", () => {
+  const addToItinerary2 = event.target.parentNode;
+  if (event.target.id === "save-button2") {
+    const itineraryPark2 = addToItinerary2.cloneNode(true);
+    document.querySelector("#resultsContainer").innerHTML = "";
+    document.querySelector("#parkItineraryContainer2").innerHTML = "";
+    document
+      .querySelector("#parkItineraryContainer2")
+      .appendChild(itineraryPark2);
+    document.querySelector("#save-button2").remove();
+    document.querySelector(".individual-park2").appendChild(removeButton2);
+  }
+});
+
+document
+  .querySelector("#parkItineraryContainer2")
+  .addEventListener("click", () => {
+    if (event.target.id === "remove-button2") {
+      document.querySelector("#parkItineraryContainer2").innerHTML = "";
     }
-    )
-
-    document.querySelector("#parkItineraryContainer2").addEventListener("click", () => {
-        if(event.target.id === "remove-button2") {
-            document.querySelector("#parkItineraryContainer2").innerHTML = "";
-        }
-    })
+  });
